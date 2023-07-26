84a1baea462bce8d79c1ad44f851117218025531
renxingwei <1697824666@qq.com>
2023-07-26 21:48:03 +0800

    init project

--- stick-common/src/main/java/com/stick/bbs/utils/ApiResponse.java
+++ stick-common/src/main/java/com/stick/bbs/utils/ApiResponse.java
@@ -9,7 +9,7 @@ import lombok.Data;
  * @param <T>
  */
 @Data
-public class Result<T> {
+public class ApiResponse<T> {
     //编码 0表示成功 其他值表示失败
     private int code = 0;
     //消息内容
@@ -17,30 +17,30 @@ public class Result<T> {
     //响应数据
     private T data;
 
-    public static <T> Result<T> ok() {
+    public static <T> ApiResponse<T> ok() {
         return ok(null);
     }
 
-    public static <T> Result<T> ok(T data) {
-        Result<T> res = new Result<>();
+    public static <T> ApiResponse<T> ok(T data) {
+        ApiResponse<T> res = new ApiResponse<>();
         res.setData(data);
         return res;
     }
 
-    public static <T> Result<T> error() {
+    public static <T> ApiResponse<T> error() {
         return error(ResultCode.FAILED);
     }
 
-    public static <T> Result<T> error(String msg) {
+    public static <T> ApiResponse<T> error(String msg) {
         return error(ResultCode.FAILED.getCode(), msg);
     }
 
-    public static <T> Result<T> error(ResultCode resultCode) {
+    public static <T> ApiResponse<T> error(ResultCode resultCode) {
         return error(resultCode.getCode(),resultCode.getMsg());
     }
 
-    public static <T> Result<T> error(int code, String msg) {
-        Result<T> res = new Result<>();
+    public static <T> ApiResponse<T> error(int code, String msg) {
+        ApiResponse<T> res = new ApiResponse<>();
         res.setCode(code);
         res.setMsg(msg);
         return res;
